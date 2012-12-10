4c56da95cfe64c7e80c1cfa2619dc78b7416d31f
Ryan Slater <b1014660@my.shu.ac.uk>
2012-12-10 17:31:23 +0000

    created intial domain

---
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
@@ -0,0 +1,8 @@
+package uk.ac.shu.webarch.eregister
+
+class Instructor {
+ String name 
+String staffNumber 
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/uk/ac/shu/webarch/eregister/RegClass.groovy
@@ -0,0 +1,9 @@
+package uk.ac.shu.webarch.eregister
+
+class RegClass {
+String name
+Instructor class_Instructor
+
+    static constraints = {
+    }
+}
